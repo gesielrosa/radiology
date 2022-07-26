60885cd027b7040afad00714a87d717acd2cd79a
gesielrosa <gesielr@gmail.com>
2022-07-25 22:11:35 -0300

    feat(client): add nebular

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NbLayoutModule, NbThemeModule} from '@nebular/theme';
+import {NbEvaIconsModule} from '@nebular/eva-icons';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({name: 'default'}),
+    NbLayoutModule,
+    NbEvaIconsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
